2d223f690786a26d419a8938378405581a6284da
Kacper-Wierzba <s30508@pjwstk.edu.pl>
2025-04-24 11:17:19 +0200

    broke all functionallity, fixed toolbar gui

--- app/src/main/java/com/example/drinkder/MainActivity.kt
+++ app/src/main/java/com/example/drinkder/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.drinkder
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.drinkder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +18,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard
-            )
+            setOf(R.id.navigation_home, R.id.navigation_dashboard)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        // Obsługa kliknięć w Twoim customowym toolbarze
+        val homeIcon = findViewById<ImageView>(R.id.nav_home)
+        val dashboardIcon = findViewById<ImageView>(R.id.nav_dashboard)
+
+        homeIcon.setOnClickListener {
+            navController.navigate(R.id.navigation_home)
+        }
+
+        dashboardIcon.setOnClickListener {
+            navController.navigate(R.id.navigation_dashboard)
+        }
     }
 }
